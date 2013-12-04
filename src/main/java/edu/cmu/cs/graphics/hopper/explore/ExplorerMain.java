afb6734e4dcfc1a1d2a42fb77373537b13357e5c
bhumbers <raichuman@gmail.com>
2013-12-04 18:26:54 -0500

    Experiments using Xstream for serialization

--- src/main/java/edu/cmu/cs/graphics/hopper/explore/ExplorerMain.java
+++ src/main/java/edu/cmu/cs/graphics/hopper/explore/ExplorerMain.java
@@ -1,5 +1,7 @@
 package edu.cmu.cs.graphics.hopper.explore;
 
+import com.thoughtworks.xstream.XStream;
+import edu.cmu.cs.graphics.hopper.problems.TerrainProblem;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.box2d.proto.Box2D;
 import org.jbox2d.common.Vec2;
@@ -8,6 +10,10 @@ import org.jbox2d.serialization.pb.PbDeserializer;
 import org.jbox2d.serialization.pb.PbSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ExplorerMain {
 
@@ -19,15 +25,20 @@ public class ExplorerMain {
         Explorer explorer = new SimpleExplorer();
         explorer.explore();
 
-        Vec2 gravity = new Vec2(0, -10f);
-        World world = new World(gravity);
-        PbSerializer serializer = new PbSerializer();
-        PbDeserializer deserializer = new PbDeserializer();
-        Box2D.PbWorld serializedWorld =  serializer.serializeWorld(world).build();
-        World world2 = deserializer.deserializeWorld(serializedWorld);
+        //Box2D world serialization/deserialization test
+//        Vec2 gravity = new Vec2(0, -10f);
+//        World world = new World(gravity);
+//        PbSerializer serializer = new PbSerializer();
+//        PbDeserializer deserializer = new PbDeserializer();
+//        Box2D.PbWorld serializedWorld =  serializer.serializeWorld(world).build();
+//        World world2 = deserializer.deserializeWorld(serializedWorld);
+//        System.out.println(world2.toString());
 
-        log.debug("Things and stuff");
-        log.info("Things and stuff");
-        System.out.println(world2.toString());
+        //Xstream serialization test
+        XStream xstream = new XStream();
+        xstream.alias("terrainproblem", TerrainProblem.class);
+        TerrainProblem blah = new TerrainProblem(new ArrayList<Float>(Arrays.asList(1.0f, 2.0f, 3.0f)), 42);
+
+        log.debug("Blah: " + xstream.toXML(blah));
     }
 }
