f415b837010762239ee4172f1c73c331235a7608
bhumbers <raichuman@gmail.com>
2013-12-13 19:49:49 -0500

    Analysis script tweak

--- analysis/scratch.m
+++ analysis/scratch.m
@@ -1,15 +1,17 @@
 %Histogram of # tests run per problem solved
 
 data1 = csv2struct('NaiveBaseline_TerrainMag4_ExpLog.csv');
-data2 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit1_ExpLog.csv');
-data3 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_ExpLog.csv');
-data4 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_SCO_ExpLog.csv');
+% data2 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit1_ExpLog.csv');
+% data3 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_ExpLog.csv');
+% data4 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_SCO_ExpLog.csv');
 
 
-for data = [data1, data2]
+for data = [data1]
     figure;
     [~, uniqueSolveLogIndices, ~] = unique(data.Num_Solved);
     plot(data.Num_Solved(uniqueSolveLogIndices), [data.Num_Tests(uniqueSolveLogIndices), data.Num_Challenges(uniqueSolveLogIndices)]);
     axis([0, 200, 0 , 1000]);
-    % hist(diff(NumTests(uniqueSolveLogIndices)))
+    
+    figure;
+    hist(diff(data.Num_Tests(uniqueSolveLogIndices)))
 end
