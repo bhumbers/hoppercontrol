223fcfcb888b229105e13f011a5f365a6ad738a2
raichuman9 <raichuman@gmail.com>
2013-12-14 13:34:15 -0500

    Some initial analysis on "big" data set (only has terrain mags 0.5 and 6.0 atm, though)

--- analysis/scratch.m
+++ analysis/scratch.m
@@ -1,17 +1,19 @@
 %Histogram of # tests run per problem solved
 
-data1 = csv2struct('NaiveBaseline_TerrainMag4_ExpLog.csv');
+data1 = csv2struct('BigRange_ExpLog.csv');
+data2 = csv2struct('BigRange_SCO_ExpLog.csv');
 % data2 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit1_ExpLog.csv');
 % data3 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_ExpLog.csv');
 % data4 = csv2struct('NaiveBaseline_TerrainMag4_EarlyExit2_SCO_ExpLog.csv');
 
 
-for data = [data1]
+for data = [data1, data2]
     figure;
     [~, uniqueSolveLogIndices, ~] = unique(data.Num_Solved);
-    plot(data.Num_Solved(uniqueSolveLogIndices), [data.Num_Tests(uniqueSolveLogIndices), data.Num_Challenges(uniqueSolveLogIndices)]);
-    axis([0, 200, 0 , 1000]);
+    plot(data.Num_Solved(uniqueSolveLogIndices), data.Num_Tests(uniqueSolveLogIndices));
+%     xlim([-1, -1])
+    ylim([0 , 1000]);
     
-    figure;
-    hist(diff(data.Num_Tests(uniqueSolveLogIndices)))
+%     figure;
+%     hist(diff(data.Num_Tests(uniqueSolveLogIndices)))
 end
